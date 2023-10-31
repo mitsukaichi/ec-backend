aca4f99ae455994619827f4213e24cbb87d1fa63
Minami Itsukaichi <mitukaichi@gmail.com>
2023-10-31 16:34:38 -0700

    add get and put to categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,22 +3,52 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+router.get('/', async (req, res) => {
+  try {
+    const categoryData = await Category.findAll({include: Product});
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get('/:id', async (req, res) => {
+  try {
+    const categoryDataById = await Category.findByPk(req.params.id,{include: Product});
+    if (categoryDataById) {
+      res.status(200).json(categoryDataById);
+    } else {
+      res.status(404).json({ message: `There is no category with this ID.` });
+    };
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+router.post('/', async (req, res) => {
+  try {
+    const createNewCateogry = await Category.bulkCreate([{ category_name: req.body.name }]);
+    res.status(200).json(createNewCateogry);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+router.put('/:id', async (req, res) => {
+  try {
+    const updateCategory = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (updateCategory[0]) {
+      res.status(200).json({ message: `The category id ${req.params.id} was updated.` });
+    } else {
+      res.status(404).json({ message: `There is no category with this ID.` });
+    };
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
