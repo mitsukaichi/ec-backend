e444237100f0f975828cc5e2e9518a176dde01ee
Minami Itsukaichi <mitukaichi@gmail.com>
2023-11-01 15:00:11 -0700

    update model index and add get all tags

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -5,14 +5,9 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 router.get('/', async (req, res) => {
   try {
-    const tagData = await Tag.findAll({include: [
-      {
-        model: ProductTag,
-        include: [
-          {model: Product},
-        ],
-      },
-    ]});
+    const tagData = await Tag.findAll({
+      include: [ { model: Product}],
+    });
     res.status(200).json(tagData);
   } catch (err) {
     res.status(500).json(err);
